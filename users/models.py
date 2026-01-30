8371db375a13d5190e99862b9396e6425be66ea3
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-30 20:26:24 -0300

    create app users

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    
+    username = None
+
+    def __str__(self):
+        return self.email
