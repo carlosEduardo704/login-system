5b6fdae9c312a6c44b6424534a34134b9db8aa97
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-01-30 23:38:00 -0300

    create CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,17 +1,19 @@
 from django.db import models
-
-# Create your models here.
+from django.core.validators import validate_email
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# Manager
+from users.managers import CustomUserBaseManager
 
 class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, validators=[validate_email])
 
-    email = models.EmailField(unique=True)
+    objects = CustomUserBaseManager()
 
     USERNAME_FIELD = 'email'
-    
-    username = None
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
