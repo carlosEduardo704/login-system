bf27da31496f15de206b728b15199d0b76ef604f
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-02-01 10:24:33 -0300

    set email configuration

--- app/settings.py
+++ app/settings.py
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Email configuration
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'carlos704estudo@gmail.com'
+EMAIL_HOST_PASSWORD = 'ktsh iyyc iuxw yxpw'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
