0fc08e7e3e25c5c2ebee9f6bff918e5952aed3eb
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-02-06 12:29:56 -0300

    refator create_token signal, using OtpToken methods to create tokens and send emails

--- users/signals.py
+++ users/signals.py
@@ -12,28 +12,9 @@ def create_token(sender, instance, created, **kwargs):
         if instance.is_superuser:
             pass
         else:
-            OtpToken.objects.create(user=instance, otp_expires_at=timezone.now() + timezone.timedelta(minutes=5))
+            OtpToken.create_new_opt_code(instance)
             instance.is_active = False
             instance.save()
 
             # Email credentials
-            otp = OtpToken.objects.filter(user=instance).last()
-            subject = 'Email Verification'
-            message = f'''
-                        Olá, {instance.email}. Here is your vefication code {otp.otp_code}.
-                        It expires in {otp.otp_expires_at - timezone.now()} minutes.
-                        Use the url to redirect back to website and verify your account.
-                        https://teste/verify_email/{instance.email}
-            '''
-
-            sender = 'carlos704estudo@gmail.com'
-            receiver = [instance.email]
-
-            # send Email
-            send_mail(
-                subject,
-                message,
-                sender,
-                receiver,
-                fail_silently=False
-            )
+            OtpToken.send_email(instance)
