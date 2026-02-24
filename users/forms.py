10435678fbe80fdeca0ba6eff9fa30234040a5e7
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-02-23 23:49:28 -0300

    add CheckemailForm, plus remove RegisterForm and ResendOtpCodeForm

--- users/forms.py
+++ users/forms.py
@@ -2,28 +2,14 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-class RegisterForm(UserCreationForm):
-    
-    password1 = forms.CharField(
-        widget=forms.PasswordInput(),
-        label='Password'
-    )
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(),
-        label='Confirm password'
-    )
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-        fields = ('email',)
+class CheckEmailForm(forms.Form):
+    email = forms.EmailField()
 
-
-    def __init__(self, *args, user=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.user = user
-
+    class Meta:
+        fields = ['email',]
 
 class OtpVerificationForm(forms.Form):
-    otp_code = forms.CharField(max_length=6, label='OTP Code:')
+    otp_code = forms.CharField(max_length=6, label='')
 
     class Meta:
         fields = ['otp_code']
@@ -33,14 +19,3 @@ class OtpVerificationForm(forms.Form):
         super().__init__(*args, **kwargs)
 
         self.fields['otp_code'].widget.attrs['placeholder'] = 'OTP Code'
-
-
-class ResendOptCodeForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(),)
-
-    class Meta:
-        fields = ['email']
-
-        labels = {
-            'email': 'Email'
-        }
