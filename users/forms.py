a0f857ba6206fc7f2577e76536c608e0d892cd76
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-02-25 09:29:37 -0300

    add clean_email to validate otp code limit per user in a day

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from users.models import OtpToken
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class CheckEmailForm(forms.Form):
     email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        user, created = get_user_model().objects.get_or_create(email=email)
+
+        # Otp_code creating limit
+        start = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        end = start + timezone.timedelta(days=1)
+
+        user_otp_create_today = OtpToken.objects.filter(user=user,
+            otp_created_at__gte=start,
+            otp_created_at__lt=end).count()
+
+        if user_otp_create_today > 3:
+            raise ValidationError(
+                "Too many attempts. You rechead the OTP code limit today. Try again tomorrow."
+            )
+        
+        return email
 
     class Meta:
         fields = ['email',]
