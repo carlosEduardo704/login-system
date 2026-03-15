b799d84f6fcf6344bf49b99037e0b2844c851124
carlosEduardo704 <carlos704eduardo@gmail.com>
2026-03-15 16:57:32 -0300

    add url_code generator limit on send_password_reset_user_email

--- users/tasks.py
+++ users/tasks.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from users.models import OtpToken, UrlCodeOtp
 import os
 from dotenv import load_dotenv
+from django.utils import timezone
 
 load_dotenv()
 
@@ -17,11 +18,24 @@ def send_password_reset_user_email(email):
     except User.DoesNotExist:
         return
 
+    # Url_code creating limit
+    start = timezone.now() - timezone.timedelta(minutes=20)
+    end = timezone.now()
+
+    user_otp_created_in_last_20_minutes = UrlCodeOtp.objects.filter(user=user,
+        created_at__gte=start,
+        created_at__lt=end
+        ).count()
+
+    if user_otp_created_in_last_20_minutes > 3:
+        print('########### Otp code limit reached ################')
+        return
+
     token = UrlCodeOtp.create_url_code(user)
     link = f'http://{os.getenv("SITE_DOMAIN")}/update_password/{email}/{token}'
     send_mail(
         subject="Reset Password",
-        message=f"Olá, {email}. Here is your link to change the password {link} . It expires in 5 minutes.",
+        message=f"Olá, {email}. Here is your link to change the password {link} . It expires in 10 minutes.",
         from_email="carlos704estudo@gmail.com",
         recipient_list=[email],
     )
